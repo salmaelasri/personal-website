33c2bfa6b8c4e3604d6e4abe922c7c926306388a
salmaelasri <110395888+salmaelasri@users.noreply.github.com>
2022-08-25 17:26:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+//about me
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+//skills
+function openNav2() {
+  document.getElementById("myNav2").style.height = "100%";
+}
+function closeNav2() {
+  document.getElementById("myNav2").style.height = "0%";
+  }
+//projects
+  function openNav3() {
+    document.getElementById("myNav3").style.height = "100%";
+  }
+  function closeNav3() {
+    document.getElementById("myNav3").style.height = "0%";
+  }
+
+ //contact
+ function openNav4() {
+  document.getElementById("myNav4").style.height = "100%";
+}
+function closeNav4() {
+  document.getElementById("myNav4").style.height = "0%";
+}   
